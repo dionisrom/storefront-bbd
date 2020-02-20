f77f929420280f99f0ff2cf033bdde12392976da
Bogdan Bajanica <dionisrom@gmail.com>
2020-02-20 14:38:19 -0500

    Update global_server.php

--- inc/global_server.php
+++ inc/global_server.php
@@ -3,7 +3,7 @@
 define("DB_SERVER", "localhost");
 define("DB_NAME", "orto");
 define ("DB_USER", "root");
-define ("DB_PASSWORD", "bobdio");
+define ("DB_PASSWORD", "mypass");
 $db=mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die ("Nu pot sa ma conectez !!! ".mysql_error());
 mysql_set_charset('utf8',$db);
 mysql_select_db(DB_NAME, $db) or die ("Nu gasesc baza de date !!! ".mysql_error());
